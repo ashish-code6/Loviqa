a59c69fc1c333b2c8008b7998ec1d97a2332877d
ashish-code6 <ashishk810222@gmail.com>
2026-08-02 18:36:24 +0530

    feat(auth): add register dto validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+  new ValidationPipe({
+    whitelist: true, // allow only dto fields
+    forbidNonWhitelisted: true,
+    transform: true, //Incoming data ko DTO ke according transform karta hai.
+  }),
+);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
