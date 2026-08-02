1ff28601dd1cb04ab05b025c1fe89ab0f300bfa9
ashish-code6 <ashishk810222@gmail.com>
2026-08-02 18:21:37 +0530

    feat(auth): add register endpoint structure

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    register(registerDto:RegisterDto){
+        return registerDto
+    }
+}
