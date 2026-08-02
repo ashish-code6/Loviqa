24e7a1c52a91e10ceb6bee0b14fa01600e1c8741
ashish-code6 <ashishk810222@gmail.com>
2026-08-02 19:40:15 +0530

    chore: setup prisma with postgres database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers:[],
 })
