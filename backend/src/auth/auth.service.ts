7a9b98f51c3cf5a32f6d45fbc3f37fc914e0f7ae
ashish-code6 <ashishk810222@gmail.com>
2026-08-04 14:14:26 +0530

    feat(auth): implement user registration with prisma

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './dto/register.dto';
+// import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Injectable()
 export class AuthService {
-    register(registerDto:RegisterDto){
-        return registerDto
+
+    constructor(private readonly prisma:PrismaService){}
+
+
+    async register(registerDto:RegisterDto){
+
+        const existingUser = await this.prisma.user.findUnique({
+            where:{
+                email:registerDto.email
+            }
+        });
+
+        if(existingUser){
+            throw new ConflictException("User already exists in db!!!")
+        }
+
+        const hashPassword = await bcrypt.hash(registerDto.password,10);
+
+        // Create a user
+
+        const user = await this.prisma.user.create({
+
+            data:{
+                name:registerDto.name,
+                email:registerDto.email,
+                password:hashPassword,
+            }
+
+        })
+
+        // remove password from response
+
+        const {password,...userWithoutPassword} =user
+
+        return{
+            message:"User Register Successfully!!",
+            user:userWithoutPassword
+        }
+       
     }
 }
